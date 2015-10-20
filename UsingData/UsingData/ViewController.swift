cb397b288f55d8e69b57d9d53b535a81d7b536c9
tingting.gu <tingting.gu@gree.co.jp>
2015-10-20 18:14:41 +0900

    init the project

---
+++ UsingData/UsingData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingData
+//
+//  Created by Tingting Gu on 2015/10/20.
+//  Copyright © 2015年 Cookrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
